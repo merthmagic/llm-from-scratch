180d79c36452c16a100441da9accf47617f0540f
yimlu <luyiming2009@gmail.com>
2024-01-03 15:44:39 +0800

    add tools.py

---
+++ tools.py
@@ -0,0 +1,30 @@
+import torch
+
+from typing import List
+
+
+def one_hot_encoding(word: str, word_to_idx: {str: int}) -> torch.Tensor:
+    tensor = torch.zeros(len(word_to_idx))
+    tensor[word_to_idx[word]] = 1
+    return tensor
+
+
+def prepare_words(sentences: [str]) -> List[str]:
+    return list(set((" ".join(sentences)).split()))
+
+
+def prepare_word_to_idx(words: List[str]) -> {str: int}:
+    return {word: idx for idx, word in enumerate(words)}
+
+
+if __name__ == "__main__":
+    corpus = ["Merlin is bird", "Tiger is cat"]
+
+    words = prepare_words(corpus)
+    print(words)
+
+    word_to_idx = prepare_word_to_idx(words)
+    print(word_to_idx)
+
+    encoded = [(one_hot_encoding(w,word_to_idx),w) for w in words]
+    print(encoded)
